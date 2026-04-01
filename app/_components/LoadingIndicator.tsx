d760c9181f291ff636d5040b4d7fe5a5593f54f3
adriandotdev <marcelonads@gmail.com>
2026-04-02 02:54:43 +0800

    add loading indicator when adding new location

---
+++ app/_components/LoadingIndicator.tsx
@@ -0,0 +1,12 @@
+export const LoadingIndicator = () => {
+	return (
+		<div className="z-9999 bg-black/50 fixed inset-0 flex items-center justify-center flex flex-col transition-all">
+			<p className="text-white font-bold mb-3">Adding Location...</p>
+			<div className="flex gap-2">
+				<div className="bg-blue-500 animate-bounce h-5 w-5 rounded-full" />
+				<div className="bg-blue-500 animate-bounce h-5 w-5 rounded-full [animation-delay:0.2s]" />
+				<div className="bg-blue-500 animate-bounce h-5 w-5 rounded-full [animation-delay:0.4s]" />
+			</div>
+		</div>
+	);
+};
