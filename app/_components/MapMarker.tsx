3d266255cdea9a072cc23c1f79c8b59caafa5f45
adriandotdev <marcelonads@gmail.com>
2026-04-02 02:16:14 +0800

    add events for centering location marker when hovered and clicked

--- app/_components/MapMarker.tsx
+++ app/_components/MapMarker.tsx
@@ -1,32 +1,129 @@
-import L from "leaflet";
-import { useState } from "react";
-import { Marker, Popup, useMapEvents } from "react-leaflet";
+import L, { LeafletMouseEvent } from "leaflet";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import { Marker, Popup, useMap, useMapEvents } from "react-leaflet";
+import { reverseGeocode } from "../_utils/reverseGeocode";
+import { useLocationStore } from "../stores/useLocationStore";
 
-const customIcon = new L.Icon({
+const markerIcon = new L.Icon({
 	iconUrl: "/MapPin.svg",
 	iconSize: [45, 45],
 	iconAnchor: [16, 32],
 	popupAnchor: [0, -32],
 });
 
-export const MapMarker = () => {
-	const [markers, setMarkers] = useState<[number, number][]>([]);
+export type TLocation = {
+	name: string;
+	coordinates: [number, number];
+};
 
+export type TMapMarkerProps = {
+	locations: TLocation[];
+	setLocations: Dispatch<SetStateAction<TLocation[]>>;
+};
+
+const handleMapClick = async (
+	e: LeafletMouseEvent,
+	setLocations: Dispatch<SetStateAction<TLocation[]>>,
+) => {
+	const { lat, lng } = e.latlng;
+
+	const displayName = await reverseGeocode(lat, lng);
+
+	setLocations((prev) => [
+		...prev,
+		{
+			name:
+				displayName.name.length > 0
+					? displayName.name
+					: displayName.display_name,
+			coordinates: [lat, lng],
+		},
+	]);
+
+	return displayName;
+};
+
+export const LocationMarker = ({
+	location,
+	index,
+}: {
+	location: TLocation;
+	index: number;
+}) => {
+	const { activeLocation } = useLocationStore();
+	const locationMarkerRef = useRef<L.Marker>(null);
+	const map = useMap();
+
+	const isDesktop = window.matchMedia("(min-width: 1280px)").matches;
+
+	const flyToWithOffset = (
+		map: L.Map,
+		lat: number,
+		lng: number,
+		offsetY: number,
+	) => {
+		const point = map.project([lat, lng], map.getZoom());
+
+		const newPoint = L.point(point.x, point.y + offsetY);
+
+		const newLatLng = map.unproject(newPoint, map.getZoom());
+
+		map.flyTo(newLatLng, map.getZoom(), {
+			animate: true,
+			easeLinearity: 3000,
+		});
+	};
+
+	useEffect(() => {
+		if (index === activeLocation) {
+			locationMarkerRef.current?.openPopup();
+
+			if (isDesktop) {
+				map.flyTo(location.coordinates, map.getZoom(), {
+					animate: true,
+					easeLinearity: 3000,
+				});
+			} else {
+				flyToWithOffset(
+					map,
+					location.coordinates[0],
+					location.coordinates[1],
+					150,
+				);
+			}
+		} else {
+			locationMarkerRef.current?.closePopup();
+		}
+	}, [activeLocation]);
+
+	return (
+		<div className="transition-transform" key={index}>
+			<Marker
+				ref={locationMarkerRef}
+				position={location.coordinates}
+				icon={markerIcon}
+			>
+				<Popup>{location.name}</Popup>
+			</Marker>
+		</div>
+	);
+};
+
+export const MapMarker = ({ locations, setLocations }: TMapMarkerProps) => {
+	const { setActiveLocation } = useLocationStore();
 	useMapEvents({
 		click(e) {
-			const { lat, lng } = e.latlng;
-
-			console.log(markers);
-			setMarkers((prev) => [...prev, [lat, lng]]);
+			void handleMapClick(e, setLocations);
+		},
+		dragstart() {
+			setActiveLocation(-1);
 		},
 	});
 
 	return (
 		<>
-			{markers.map((position, index) => (
-				<Marker key={index} position={position} icon={customIcon}>
-					<Popup>Marker {index + 1} 📍</Popup>
-				</Marker>
+			{locations.map((location, index) => (
+				<LocationMarker key={index} location={location} index={index} />
 			))}
 		</>
 	);
